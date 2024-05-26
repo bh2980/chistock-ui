9f6467391ca729256f36af093d65329a9d578c5d
bh2980 <bh2980@naver.com>
2024-05-26 17:04:16 +0900

    💄 style(css): bandAxisVariants에 base만 있도록 수정

--- src/components/charts/BandAxis/BandAxis.styles.ts
+++ src/components/charts/BandAxis/BandAxis.styles.ts
@@ -1,13 +1,5 @@
 import { tv } from "@utils/customTV";
 
 export const BandAxisVariants = tv({
-  variants: {
-    lineHide: {
-      true: "stroke-none",
-      false: "stroke-black",
-    },
-  },
-  defaultVariants: {
-    lineHide: false,
-  },
+  base: "stroke-black",
 });
