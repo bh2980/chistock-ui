e3d31f779b232dc370ce3653cc66e656356fd209
bh2980 <bh2980@naver.com>
2024-05-27 16:20:20 +0900

    💄 style(type): barChartDataType에 label을 string으로 수정 및 value에 null 제거, NullableBarChartType 추가

--- src/components/charts/BarChart/BarChart.types.ts
+++ src/components/charts/BarChart/BarChart.types.ts
@@ -1,12 +1,14 @@
 import type { AxisOrient } from "@charts/BandAxis";
 
-type BarChartDataType = { label: number; value: number | null };
+export type BarChartDataType = { label: string; value: number };
+
+export type NullableBarChartType = BarChartDataType | { label: string; value: number | null };
 
 export type BarChartProps = {
   orient?: AxisOrient;
   width: number;
   height: number;
-  data: BarChartDataType[];
+  data: NullableBarChartType[];
   padding?: number;
   showLabel?: boolean;
   labelOffset?: number;
