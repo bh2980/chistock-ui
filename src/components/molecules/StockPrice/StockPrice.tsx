b1462e26f7e352ffae040421b03853f0159d6e9e
bh2980 <bh2980@naver.com>
2024-05-27 18:43:46 +0900

    💄 style(css): className이 undefined일 경우 빈 문자열이 들어가도록 수정

--- src/components/molecules/StockPrice/StockPrice.tsx
+++ src/components/molecules/StockPrice/StockPrice.tsx
@@ -15,7 +15,7 @@ const StockPrice = ({
   const isNumber = (char: string) => Boolean(char.match(/[0-9]/));
 
   return (
-    <div className={`flex whitespace-pre ${className}`} {...props}>
+    <div className={`flex whitespace-pre ${className ? className : ""}`} {...props}>
       {Array.from(getFormatNumber({ number: price, decimalPoint, prefix, postfix })).map((str, i) => (
         <If condition={animated && isNumber(str)} key={`sp-${i}`}>
           <Then>
