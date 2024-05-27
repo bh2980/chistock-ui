58b51601ee78084f630c53e7779efd224612ef93
bh2980 <bh2980@naver.com>
2024-05-27 16:20:49 +0900

    📝 docs(story): label을 string으로 수정

--- src/components/charts/BarChart/BarChart.stories.tsx
+++ src/components/charts/BarChart/BarChart.stories.tsx
@@ -14,11 +14,11 @@ export default meta;
 type Story = StoryObj<typeof BarChart>;
 
 const data = [
-  { label: 2019, value: 151.8 },
-  { label: 2020, value: 242.2 },
-  { label: 2021, value: 121.3 },
-  { label: 2022, value: 200.7 },
-  { label: 2023, value: null }, // 값이 아직 없는 경우
+  { label: "2019", value: 151.8 },
+  { label: "2020", value: 242.2 },
+  { label: "2021", value: 121.3 },
+  { label: "2022", value: 200.7 },
+  { label: "2023", value: null }, // 값이 아직 없는 경우
 ];
 
 export const Default: Story = {
