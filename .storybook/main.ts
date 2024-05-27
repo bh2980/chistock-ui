90211f4089e374d7b7c243c5bbda3838731e9175
bh2980 <bh2980@naver.com>
2024-05-27 19:28:22 +0900

    🚚 chore(config): storybook에 a11y 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,6 +10,7 @@ const config: StorybookConfig = {
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
     "@storybook/addon-themes",
+    "@storybook/addon-a11y",
     "storybook-addon-pseudo-states",
   ],
   framework: {
